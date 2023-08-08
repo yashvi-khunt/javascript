7d225919b8ee3cf71c309abace0121d1ab793ad7
yashvi-khunt <yashvi@weybee.in>
2023-08-08 11:05:59 +0530

    Completed coding challenge 4 of javascript fundamentals part-2

--- script.js
+++ script.js
@@ -33,11 +33,12 @@ const calcTip = (bill) => {
     tips.push(tip);
     totals.push(bill+tip);
 }
+
 function cc2(){
     // for(let i = 0; i<bills.length; i++){
     //     calcTip(bills[i]);
     // }
-    bills.forEach(bill => calcTip(bill) )
+    bills[calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])]
        
     console.log(bills)
     console.log(tips)
@@ -51,7 +52,9 @@ const mark = {
     height: 1.69,
     calcBMI: function(){
         this.bmi = this.weigth / this.height**2;
+        console.log(this.weigth, this.height)
         return this.bmi;
+        
     }
 }
 
@@ -67,12 +70,48 @@ const john = {
 }
 
 const cc3 = () => {
-    mark.calcBMI()
-    john.calcBMI();
-    if(mark.bmi > john.bmi){
-        console.log(`${mark.firstName} ${mark.lastName}'s BMI (${mark.bmi}) is higher than ${john.firstName} ${john.lastName}'s (${john.bmi})`);
+    // mark.calcBMI()
+    // john.calcBMI();
+
+    if(mark.calcBMI() > john.calcBMI() ){
+        console.log(`${mark.firstName} ${mark.lastName}'s BMI (${mark.calcBMI()}) is higher than ${john.firstName} ${john.lastName}'s (${john.calcBMI()})`);
     }else{
-        console.log(`${john.firstName} ${john.lastName}'s BMI (${john.bmi}) is higher than ${mark.firstName} ${mark.lastName}'s (${mark.bmi})`);
+        console.log(`${john.firstName} ${john.lastName}'s BMI (${john.calcBMI()}) is higher than ${mark.firstName} ${mark.lastName}'s (${mark.calcBMI()})`);
     }
 
+}
+//coding challenge 4
+
+const bills4 = [22,295,176,440,37,105,10,1100,86,52]
+let totals4 = [];
+let tips4 = [];
+
+const calcTip4 = (bill) => {
+    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+    tips4.push(tip);
+    totals4.push(bill+tip);
+}
+
+const calcAverage4 = (arr) => {
+    let sum=0;
+    for(let i=0; i<arr.length; i++){
+        sum += arr[i];
+    }
+
+    const avg = sum / arr.length;
+
+    return(`Average: ${avg}`);
+}
+
+const cc4 = function(){
+    for(let i = 0; i<bills4.length; i++){
+        calcTip4(bills4[i])
+    }
+
+    console.log(`Bills : ${bills4}`);
+    console.log(`Tips : ${tips4}`);
+    console.log(`Totals : ${totals4}`);
+
+    
+    console.log(`Average totals: ${calcAverage4(totals4)}`)
 }
